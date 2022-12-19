dac146c5d998f8ff9acd6452303da5971ef5f9ab
jackz070 <95173354+jackz070@users.noreply.github.com>
2022-12-19 20:06:27 +0100

    Update readme.md

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageNotFound = () => {
   return (
-    <div className="w-full min-h-screen flex justify-center items-center flex flex-col">
+    <div className="w-full min-h-screen justify-center items-center flex flex-col">
       <div>404</div>
       <div>Something went wrong...</div>
     </div>
